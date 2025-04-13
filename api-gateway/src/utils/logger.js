4f2ff73bc1db7ab31ce96445cfc1ba950fd2ecde
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-04-13 22:42:06 +0530

    Add logger utility using Winston for structured logging in API Gateway

---
+++ api-gateway/src/utils/logger.js
@@ -0,0 +1,32 @@
+const { createLogger, format, transports } = require('winston');
+const chalk = require('chalk');
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(({ timestamp, level, message }) => {
+            let consoleMessage = `[node] - ${timestamp} ${level.toUpperCase()} [API-GATEWAY] ${message}`;
+            let colouredConsoleMessage;
+
+            switch (level) {
+                case 'info':
+                    colouredConsoleMessage = chalk.green(consoleMessage);
+                    break;
+                case 'warn':
+                    colouredConsoleMessage = chalk.yellow(consoleMessage);
+                    break;
+                case 'error':
+                    colouredConsoleMessage = chalk.red(consoleMessage);
+                    break;
+                default:
+                    colouredConsoleMessage = consoleMessage;
+            }
+
+            return colouredConsoleMessage;
+        })
+    ),
+    transports: [new transports.Console()],
+});
+
+module.exports = logger;
