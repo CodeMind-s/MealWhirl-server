afc0bd08ff51e487e92f315ced750fab272d2b61
Muhammed Arshaq <mjmarshaq@gmail.com>
2025-04-26 00:14:52 +0530

    feat: add notification routes to API gateway

--- api-gateway/src/routes.js
+++ api-gateway/src/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const orderRoutes = require("./routes/orderRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 /**
@@ -12,6 +13,7 @@ const router = Router();
 router.use("/orders", orderRoutes);
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/notifications", notificationRoutes);
 router.use("/health", (req, res) => {
   res.status(200).send("API gateway is healthy");
 });
