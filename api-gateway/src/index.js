b1d6090576c1742d84fe1a177c96f760a18b1fc7
Muhammed Arshaq <mjmarshaq@gmail.com>
2025-04-26 00:14:46 +0530

    fix: refactor index.js for improved readability and consistency

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -7,81 +7,88 @@ const httpContext = require("express-http-context");
 require("dotenv").config();
 const errorHandler = require("./middlewares/errorHandler");
 const tokenHandler = require("./middlewares/tokenHandler");
-const $404Handler = require('./middlewares/404Handler');
+const $404Handler = require("./middlewares/404Handler");
 const errorLogger = require("./middlewares/errorLogger");
 const logger = require("./utils/logger");
 const { logStream } = require("./utils/logger");
-const {initKafkaProducer, initKafkaConsumer} = require("./services/kafkaService");
+const {
+  initKafkaProducer,
+  initKafkaConsumer,
+} = require("./services/kafkaService");
 const router = require("./routes");
 const {
-    BASE_URL,
-    APP_PORT,
-    APP_HOST,
-    KEEP_ALIVE_TIME_OUT,
-    HEADERS_TIME_OUT, IMMEDIATE_LOG_FORMAT, LOG_FORMAT
+  BASE_URL,
+  APP_PORT,
+  APP_HOST,
+  KEEP_ALIVE_TIME_OUT,
+  HEADERS_TIME_OUT,
+  IMMEDIATE_LOG_FORMAT,
+  LOG_FORMAT,
 } = require("./constants/configConstants");
 
 const startServer = async () => {
-    try {
-        await initKafkaProducer();
-        logger.info("Kafka Producer initialized");
-        
-        await initKafkaConsumer();
-        logger.info("Kafka Consumer initialized");
+  try {
+    await initKafkaProducer();
+    logger.info("Kafka Producer initialized");
 
-        const app = express();
-        app.set("port", APP_PORT);
-        app.set("host", APP_HOST);
+    await initKafkaConsumer();
+    logger.info("Kafka Consumer initialized");
 
-        app.use(cors());
-        app.use(helmet());
-        app.use(compression());
-        app.use(httpContext.middleware);
+    const app = express();
+    app.set("port", APP_PORT);
+    app.set("host", APP_HOST);
 
-        app.use(
-            morgan(IMMEDIATE_LOG_FORMAT, {
-                immediate: true,
-                stream: logStream
-            })
-        );
-        app.use(
-            morgan(LOG_FORMAT, {
-                stream: logStream
-            })
-        );
+    app.use(cors());
+    app.use(helmet());
+    app.use(compression());
+    app.use(httpContext.middleware);
 
-        app.use(tokenHandler());
+    app.use(
+      morgan(IMMEDIATE_LOG_FORMAT, {
+        immediate: true,
+        stream: logStream,
+      })
+    );
+    app.use(
+      morgan(LOG_FORMAT, {
+        stream: logStream,
+      })
+    );
 
-        app.use(BASE_URL, router);
+    app.use(tokenHandler());
 
-        app.use($404Handler);
+    app.use(BASE_URL, router);
 
-        app.use(errorLogger());
+    app.use($404Handler);
 
-        app.use(errorHandler());
+    app.use(errorLogger());
 
-        const server = app.listen(app.get("port"), app.get("host"), () => {
-            logger.info(
-                `Server started listening at: http://${app.get("host")}:${app.get("port")}${BASE_URL}`
-            );
-        });
+    app.use(errorHandler());
 
-        server.keepAliveTimeout = KEEP_ALIVE_TIME_OUT;
-        server.headersTimeout = HEADERS_TIME_OUT;
-    } catch (error) {
-        logger.error("Failed to start API Gateway:", error);
-        process.exit(1);
-    }
-}
+    const server = app.listen(app.get("port"), app.get("host"), () => {
+      logger.info(
+        `Server started listening at: http://${app.get("host")}:${app.get(
+          "port"
+        )}${BASE_URL}`
+      );
+    });
+
+    server.keepAliveTimeout = KEEP_ALIVE_TIME_OUT;
+    server.headersTimeout = HEADERS_TIME_OUT;
+  } catch (error) {
+    logger.error("Failed to start API Gateway:", error);
+    process.exit(1);
+  }
+};
 
 process.on("unhandledRejection", (reason, promise) => {
-    logger.error("Unhandled Rejection at:", promise, "reason:", reason);
-    process.exit(1);
+  logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
 });
 
 process.on("uncaughtException", (err) => {
-    logger.error("Uncaught exception", err);
-    process.exit(1);
+  logger.error("Uncaught exception", err);
+  process.exit(1);
 });
 
 startServer().then(() => logger.info("API Gateway started successfully"));
